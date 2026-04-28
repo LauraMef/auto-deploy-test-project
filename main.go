36825df94649f13ec2d6f4693fae6aa9e766d4dc
LekeneCedric <lekenelucprofessionel@gmail.com>
2026-04-28 17:30:27 +0100

    init

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from th AutoDeploy Tes Labr 🚀")
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/", HelloHandler)
+
+	fmt.Printf("Server starting on port %s...\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
+}
